1626160f25842752469597ab5f05462eb4a6f096
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-06-07 11:05:58 +0200

    fetch SLACK_API_TOKEN from ENV in (dev|prod).exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :jaqen, api_token: "API_TOKEN"
+config :jaqen, api_token: {:system, "SLACK_API_TOKEN"}
 
 if File.exists?("config/dev.secret.exs") do
   import_config "#{Mix.env}.secret.exs"
